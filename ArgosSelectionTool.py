9fd548cb84d694b53e84a1ad8021144a54c8f576
sarahj-hall <sarah.j.hall@duke.edu>
2026-09-17 13:01:08 -0400

    Task 3 - parse a single line of tracking data

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -7,11 +7,29 @@
 # 
 # Author: Sarah Hall (sarah.j.hall@duke.edu)
 # Date:   Fall 2026 
+#------------------------------------------
 
-#create the geographic selection box 
+# Create the geographic selection box 
 the_box = {
     'x_min' : 34.00,
     'y_min' : -76.00,
     'x_max' : 34.50,
     'y_max' : -75.00
 }
+
+# copy and paste a line of data as the lineString variable value
+lineString = '10154641232,true,2019-05-14 13:37:52.000,-75.49356999999998,34.86216,,0.0,-127.0,4.0167976787E8,5141.0,424,"40",34.86216,34.86216,"0",-75.49356999999998,-75.49356999999998,6,0,3,61.0,381.0,10718.0,2466.0,150,187,2,0,"1",,,"argos-doppler-shift","Pterodroma hasitata","174441","HA09","Satellite tracking of black-capped petrels, 2019"'
+
+# use the split command to parse the items in lineString into a list object
+line_data = lineString.split(',')
+
+#assign variables to specific items in the list 
+event_id = line_data[0]  # Argos tracking event ID ("event-id")
+timestamp = line_data[2] # Observation data ("timestamp")
+lat = line_data[4]       # Observation latitude ("location-lat")
+lon = line_data[3]       # Observation longitude ("location-lon")
+lc = line_data[14]        # Observation location class ("argos:lc")
+tag_id = line_data[34]    # Tag identifier ("tag-local-identifier")
+
+#print information to use 
+print(f"Record {event_id} indicates {tag_id} was seen at {lat}N and {lon}W on {timestamp}")
